a7c926e04651674a751a9caddaf4709a03715d33
Dylan Turner <dylan.turner22@gmail.com>
2013-01-06 22:34:25 -0700

    messing with jquery and dom

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
 		var searchTerm = $('#term').val();
 
 		$("#loading").removeClass("hide");
+		$("#table2").remove();
 
 		$.getJSON("http://openstates.org/api/v1/bills/?q=" + searchTerm + "&state=ut&apikey=c13dee9099be4512a8bca6ad4f94c4aa&callback=?", function(json) {
 
@@ -14,16 +15,15 @@ $(document).ready(function() {
 					if (i === 0) {
 						$('#table').append('<thead id="table2"><tr><th>#</th><th>title</th><th>bill Id</th><th>created</th><th>subjects</th></tr></thead>');
 					} else {
-						$('#table2').append('<tr><td>' + i + '</td><td>' + JSON.stringify(json[i].title) + '</td><td>' + JSON.stringify(json[i].bill_id) +'</td>' + '</th><td>' + JSON.stringify(json[i].created_at) + '</th><td>' + JSON.stringify(json[i].subjects) +'<td></tr>');
+						$('#table2').append('<tr><td>' + i + '</td><td>' + JSON.parse(JSON.stringify(json[i].title)) + '</td><td>' + JSON.parse(JSON.stringify(json[i].bill_id)) +'</td>' + '</th><td>' + JSON.parse(JSON.stringify(json[i].created_at)) + '</th><td>' + JSON.parse(JSON.stringify(json[i].subjects)) +'<td></tr>');
 					}
 
 				}
 			} else {
-				$("#table").append('<h2 class="loading">We\'re afraid nothing was found for that search. Try again.');
+				$(".table").html('<h2 class="loading">We\'re afraid nothing was found for that search. Try again.');
 			}
 		});
 
-		$("#loading").addClass("hide");
 	};
 
 	$('#search').click(getBill);
