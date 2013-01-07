1224b3bb53fe14db4520e15edd16b7ed3fccd92d
Dylan Turner <dylan.turner22@gmail.com>
2013-01-06 17:23:17 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	
+	var getBill = function () {
+
+		var searchTerm = $('#term').val();
+
+		
+
+		$.getJSON("http://openstates.org/api/v1/bills/?q=" + searchTerm + "&state=ut&apikey=c13dee9099be4512a8bca6ad4f94c4aa&callback=?", function(json) {
+
+			if (json !== '') {
+
+				for (var i = 0; i < json.length; i++) {
+					if (i === 0) {
+						$('#table').append('<thead id="table2"><tr><th>#</th><th>title</th><th>bill Id</th><th>created at</th><th>subjects</th></tr></thead>');
+					} else {
+						$('#table2').append('<tr><td>' + i + '</td><td>' + JSON.stringify(json[i].title) + '</td><td>' + JSON.stringify(json[i].bill_id) +'</td>' + '</th><td>' + JSON.stringify(json[i].created_at) + '</th><td>' + JSON.stringify(json[i].subjects) +'<td></tr>');
+					}
+
+				}
+			} else {
+				$("#table").html('<h2 class="loading">We\'re afraid nothing was found for that search. Try again.');
+			}
+		});
+
+		
+	};
+
+	$('#search').click(getBill);
+	$('#term').keyup(function(event){
+		if(event.keyCode == 13){
+			getBill();
+    }
+	});
+
+});
