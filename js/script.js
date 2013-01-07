5cdb05a5247de17bd868c00e238a0e54d0f4413e
Dylan Turner <dylan.turner22@gmail.com>
2013-01-06 21:17:44 -0700

    added validations

--- js/script.js
+++ js/script.js
@@ -4,26 +4,26 @@ $(document).ready(function() {
 
 		var searchTerm = $('#term').val();
 
-		
+		$("#loading").removeClass("hide");
 
 		$.getJSON("http://openstates.org/api/v1/bills/?q=" + searchTerm + "&state=ut&apikey=c13dee9099be4512a8bca6ad4f94c4aa&callback=?", function(json) {
 
-			if (json !== '') {
+			if (json.length !== 0) {
 
 				for (var i = 0; i < json.length; i++) {
 					if (i === 0) {
-						$('#table').append('<thead id="table2"><tr><th>#</th><th>title</th><th>bill Id</th><th>created at</th><th>subjects</th></tr></thead>');
+						$('#table').append('<thead id="table2"><tr><th>#</th><th>title</th><th>bill Id</th><th>created</th><th>subjects</th></tr></thead>');
 					} else {
 						$('#table2').append('<tr><td>' + i + '</td><td>' + JSON.stringify(json[i].title) + '</td><td>' + JSON.stringify(json[i].bill_id) +'</td>' + '</th><td>' + JSON.stringify(json[i].created_at) + '</th><td>' + JSON.stringify(json[i].subjects) +'<td></tr>');
 					}
 
 				}
 			} else {
-				$("#table").html('<h2 class="loading">We\'re afraid nothing was found for that search. Try again.');
+				$("#table").append('<h2 class="loading">We\'re afraid nothing was found for that search. Try again.');
 			}
 		});
 
-		
+		$("#loading").addClass("hide");
 	};
 
 	$('#search').click(getBill);
