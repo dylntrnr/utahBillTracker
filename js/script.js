905d36a2cb0f1bf4ccbc61d5979b32493145c828
Dylan Turner <dylan.turner22@gmail.com>
2013-01-07 23:01:10 -0700

    added autocomplete functionality - takes a long time to work

--- js/script.js
+++ js/script.js
@@ -1,12 +1,53 @@
 $(document).ready(function() {
-
-	$('#term').typeahead({
-		source: function (query, process) {
-			options = [];
-			var data = $.getJSON("http://openstates.org/api/v1/bills/?state=ut&fields=first_name&apikey=c13dee9099be4512a8bca6ad4f94c4aa&callback=?");
-			console.log(data);
-		}
-	});
+	
+	var autocomplete = $('#term').typeahead()
+        .on('keyup', function(ev){
+ 
+            ev.stopPropagation();
+            ev.preventDefault();
+ 
+            //filter out up/down, tab, enter, and escape keys
+            if( $.inArray(ev.keyCode,[40,38,9,13,27]) === -1 ){
+ 
+                var self = $(this);
+                
+                //set typeahead source to empty
+                self.data('typeahead').source = [];
+ 
+                //active used so we aren't triggering duplicate keyup events
+                if( !self.data('active') && self.val().length > 0){
+ 
+                    self.data('active', true);
+ 
+                    //Do data request. Insert your own API logic here.
+                    $.getJSON("http://openstates.org/api/v1/bills/?state=ut&apikey=c13dee9099be4512a8bca6ad4f94c4aa&callback=?",{
+                        q: $(this).val()
+                    }, function(data) {
+ 
+                        //set this to true when your callback executes
+                        self.data('active',true);
+ 
+                        //Filter out your own parameters. Populate them into an array, since this is what typeahead's source requires
+                        var arr = [],
+                            i=data.length;
+                        while(i--){
+                            arr[i] = data[i].title
+                        }
+ 
+                        //set your results into the typehead's source
+                        self.data('typeahead').source = arr;
+ 
+                        //trigger keyup on the typeahead to make it search
+                        self.trigger('keyup');
+ 
+                        //All done, set to false to prepare for the next remote query.
+                        self.data('active', false);
+ 
+                    });
+ 
+                }
+            }
+        });
 	
 	var getBill = function () {
 
