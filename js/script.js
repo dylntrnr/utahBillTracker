9a3fc86a01363ce49705db0820283aa7defa882b
Dylan Turner <dylan.turner22@gmail.com>
2013-01-08 19:42:21 -0700

    got search function to work faster

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+stuff = [];
+
 $(document).ready(function() {
 	
+	$.getJSON("http://openstates.org/api/v1/bills/?q=utah&state=ut&fields=title&apikey=c13dee9099be4512a8bca6ad4f94c4aa&callback=?", function (json) {
+		stuff.push(json);
+	});
+
 	// This is all @geuis ---- lifesaver! 
 	var autocomplete = $('#term').typeahead()
         .on('keyup', function(ev){
@@ -21,20 +27,18 @@ $(document).ready(function() {
                     self.data('active', true);
  
                     //Do data request. Insert your own API logic here.
-                    $.getJSON("http://openstates.org/api/v1/bills/?state=ut&apikey=c13dee9099be4512a8bca6ad4f94c4aa&callback=?",{
-                        q: $(this).val()
-                    }, function(data) {
+                    $.grep(stuff, function() {
  
                         //set this to true when your callback executes
                         self.data('active',true);
  
                         //Filter out your own parameters. Populate them into an array, since this is what typeahead's source requires
+ 						
                         var arr = [],
-                            i=data.length;
+                            i=stuff[0].length;
                         while(i--){
-                            arr[i] = data[i].title
+                            arr[i] = stuff[0][i].title
                         }
- 
                         //set your results into the typehead's source
                         self.data('typeahead').source = arr;
  
@@ -45,7 +49,6 @@ $(document).ready(function() {
                         self.data('active', false);
  
                     });
- 
                 }
             }
         });
